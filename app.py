95a774a1e04bd587245bc1b91f05958975256057
Amritpal9854343 <126471012+Amritpal9854343@users.noreply.github.com>
2023-07-29 17:27:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import pandas as pd
 import pickle
 
 # import the Model
-pipe = pickle.load(open('pipe.pkl','rb'))
-df = pickle.load(open('df.pkl','rb'))
-
+pipe = pd.read_pickle(open('pipe.pkl','rb'))
+df = pd.read_pickle(open('df.pkl','rb'))
 
 st.title("Laptop Predictor")
 
